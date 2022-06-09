7b2d3d850efe71658bb6f270cba63c55eaff4a3e
Peter Wong <peter.wm.wong@gmail.com>
2022-06-09 16:42:36 -0500

    Initial Commit

---
+++ x-game/x_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  x_gameApp.swift
+//  x-game
+//
+//  Created by Peter Wong on 6/9/22.
+//
+
+import SwiftUI
+
+@main
+struct x_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
