b3c7904f1a792570a44c0a735c0f5140f6657b21
Peter Wong <peter.wm.wong@gmail.com>
2022-06-10 12:55:12 -0500

    WIP

--- x-game/Renderer.swift
+++ x-game/Renderer.swift
@@ -1,30 +1,12 @@
 import SwiftUI
 import Metal
 
-let SOURCE = """
-    #include <metal_stdlib>
-    
-    using namespace metal;
-    
-    struct VertexOut {
-        float4 position [[position]];
-        float point_size [[point_size]];
-    };
-    
-    vertex VertexOut
-    main_vertex(constant ulong & frame_num [[buffer(0)]]) {
-        return {
-            .position = float4(0,0,0,1),
-            .point_size = float(frame_num)
-        };
-    }
-    
-    fragment half4
-    main_fragment(const float2 pos [[point_coord]]) {
-        float const sd = round(1.0 - length(pos - 0.5));
-        return half4(0, 1, 0, 1) * sd;
-    }
-    """
+enum Mode {
+    case harvest_archive_binary
+    case use_archive_binary
+}
+
+let MODE = Mode.harvest_archive_binary
 
 struct Renderer {
     let device: MTLDevice
@@ -32,19 +14,31 @@ struct Renderer {
     let renderPipeline: MTLRenderPipelineState
     
     public init(device: MTLDevice) throws {
-        self.device = device
-        self.commandQueue = device.makeCommandQueue()!
-        do{
-            let lib = try device.makeLibrary(source: SOURCE, options: nil)
-            let desc = MTLRenderPipelineDescriptor()
-            desc.vertexFunction = lib.makeFunction(name: "main_vertex")
-            desc.fragmentFunction = lib.makeFunction(name: "main_fragment")
-            desc.colorAttachments[0]?.pixelFormat = .bgra8Unorm
-            self.renderPipeline = try device.makeRenderPipelineState(descriptor: desc)
-        }
-        catch {
-            print(error)
-            throw error
+       self.device = device
+       self.commandQueue = device.makeCommandQueue()!
+        
+        let lib = device.makeDefaultLibrary()!
+        lib.label = "Library"
+
+        let desc = MTLRenderPipelineDescriptor()
+        desc.vertexFunction = lib.makeFunction(name: "vert_main")
+        desc.fragmentFunction = lib.makeFunction(name: "frag_main")
+        desc.colorAttachments[0]?.pixelFormat = .bgra8Unorm
+
+        let archdesc = MTLBinaryArchiveDescriptor()
+        
+        switch MODE {
+        case .harvest_archive_binary:
+            let archive = try device.makeBinaryArchive(descriptor: archdesc)
+            try archive.addRenderPipelineFunctions(descriptor: desc)
+            try archive.serialize(to: NSURL.fileURL(withPath: "/Users/pwong/Downloads/x-game.metallib"))
+            renderPipeline = try device.makeRenderPipelineState(descriptor: desc)
+        
+        case .use_archive_binary:
+            archdesc.url = NSURL.fileURL(withPath: "/Users/pwong/projects/x-gpubin/archive-with-air.metallib")
+            let archive = try device.makeBinaryArchive(descriptor: archdesc)
+            desc.binaryArchives = [archive];
+            renderPipeline = try! device.makeRenderPipelineState(descriptor: desc, options: [.failOnBinaryArchiveMiss]).0
         }
     }
     
